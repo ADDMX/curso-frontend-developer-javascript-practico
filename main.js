fb72d4d3efb9d0c0daa976cfe695e209636b3ebb
Rafael Ivan <rafaelreyesvazquez45@gmail.com>
2023-04-08 13:49:32 -0600

    agregando

--- main.js
+++ main.js
@@ -13,14 +13,107 @@ const mobileMenu = document.querySelector(".mobile-menu");
 menuHamburgesa.addEventListener("click", muestramobileMenu);
 
 function muestramobileMenu() {
+  const isasideClosed = aside.classList.contains("inactive");
+
+  if (!isasideClosed) {
+    aside.classList.add("inactive");
+  }
+
   mobileMenu.classList.toggle("inactive");
 }
 
-const navbarShoppingCart = document.querySelector(".navbar-shopping-cart");
+// Función de ocultar y mostrar el menu del icono carrito de compras
+
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector(".product-detail");
 
-navbarShoppingCart.addEventListener("click", togglecarritoAside);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
-function togglecarritoAside() {
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+
   aside.classList.toggle("inactive");
+}
+
+// Creando carrito de compras desde javascritpt
+
+const cardsContainer = document.querySelector(".cards-container");
+
+const productList = [];
+
+productList.push({
+  name: "bike",
+  price: 120,
+  Image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "pantallas",
+  price: 20800,
+  Image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "comedor",
+  price: 11500,
+  Image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+/*
+<div class="product-card">
+        <img
+          src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div>
+*/
+
+for (product of productList) {
+  const productCard = document.createElement("div");
+  productCard.classList.add("product-card");
+
+  const productImg = document.createElement("img");
+  productImg.setAttribute("src", product.Image);
+
+  const productInfo = document.createElement("div");
+  productInfo.classList.add("product-info");
+
+  const productInfoDiv = document.createElement("div");
+
+  const productPrice = document.createElement("p");
+  productPrice.innerText = "$" + product.price;
+  const productName = document.createElement("p");
+  productName.innerText = product.name;
+
+  productInfoDiv.appendChild(productPrice);
+  productInfoDiv.appendChild(productName);
+
+  const productInfoFigure = document.createElement("figure");
+  productImgCart = document.createElement("img");
+  productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+  productInfoFigure.appendChild(productImgCart);
+
+  productInfo.appendChild(productInfoDiv);
+  productInfo.appendChild(productInfoFigure);
+
+  productCard.appendChild(productImg);
+  productCard.appendChild(productInfo);
+
+  cardsContainer.appendChild(productCard);
 }
